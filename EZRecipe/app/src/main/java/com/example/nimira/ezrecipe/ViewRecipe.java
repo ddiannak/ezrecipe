15aa1ee8e99d83e8004553c46680205e657ec43a
Dianna K <dikwan@ucsc.edu>
2017-05-25 15:24:38 -0700

    Can open selected recipe in a web browser

---
+++ EZRecipe/app/src/main/java/com/example/nimira/ezrecipe/ViewRecipe.java
@@ -0,0 +1,13 @@
+package com.example.nimira.ezrecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_recipe);
+    }
+}
